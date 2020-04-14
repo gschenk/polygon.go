454f1f9544c3e0f2699b79b262cf2c2206662ab3
gerald schenk <gschenk@gmx.net>
2020-04-14 19:12:48 +0200

    adds simple vector functions

--- vector/vector.go
+++ vector/vector.go
@@ -1,6 +1,8 @@
 // Package vector provides a type and functions for simple vector calculations
 package vector
 
+import "math"
+
 // PointXY type for a point in a Carthesian plane
 type PointXY struct {
 	X float64
@@ -16,8 +18,54 @@ func FromStruct(r PointXY) Vec {
 	return Vec{r.X, r.Y}
 }
 
+// Sum returns the sum of two vectors
+func Sum(a, b Vec) Vec {
+	return Vec{
+		a[0] + b[0],
+		a[1] + b[1],
+	}
+}
+
+// FromAtoB returns a vector from point A to point B where these points are
+// given by their position vectors
+// FromAtoB :: Vec -> Vec -> Vec
+func FromAtoB(a, b Vec) Vec {
+	return Vec{
+		b[0] - a[0],
+		b[1] - a[1],
+	}
+}
+
+// ScalarMult returns the product of a scalar with a vector
+// ScalarMult :: float64 -> Vec -> Vec
+func ScalarMult(x float64, a Vec) Vec {
+	return Vec{
+		x * a[0],
+		x * a[1],
+	}
+}
+
+// Dot returns the dot product of two vectors, a and b
+// Dot :: Vec -> Vec -> float64
+func Dot(a, b Vec) float64 {
+	return a[0]*b[0] + a[1]*b[1]
+}
+
 // Det returns the determinant of a matrix formed by the elements
 // of two column vectors provied as Vec.
+// Dot :: Vec -> Vec -> float64
 func Det(a, b Vec) float64 {
 	return a[0]*b[1] - a[1]*b[0]
+}
+
+// NormSquare returns the squared norm of a vector (dot product with itself)
+// NormSquare :: Vec -> float64
+func NormSquare(a Vec) float64 {
+	return math.Pow(a[0], 2) + math.Pow(a[1], 2)
+}
+
+// Norm returns the norm of a vector
+// Norm :: Vec -> float64
+func Norm(a Vec) float64 {
+	return math.Sqrt(NormSquare(a))
 }
--- vector/vector_test.go
+++ vector/vector_test.go
@@ -2,6 +2,14 @@ package vector
 
 import "testing"
 
+// test vectors
+var zero = Vec{0, 0}
+var unitX = Vec{1, 0}
+var unitY = Vec{0, 1}
+var vecA = Vec{2, 1}
+var vecB = Vec{1, 2}
+var vecC = Vec{-3.4, 2.5}
+
 func TestFromStruct(t *testing.T) {
 	testPoint := PointXY{X: 1.01e3, Y: 0.02}
 	result := FromStruct(testPoint)
@@ -16,27 +24,92 @@ func TestFromStruct(t *testing.T) {
 	}
 }
 
+func TestSum(t *testing.T) {
+	tables := []struct {
+		a Vec
+		b Vec
+		r Vec
+	}{
+		{vecA, vecA, Vec{4, 2}},
+		{vecA, vecB, Vec{3, 3}},
+		{vecB, vecA, Vec{3, 3}},
+		{vecA, vecC, Vec{-1.4, 3.5}},
+	}
+
+	for _, table := range tables {
+		result := Sum(table.a, table.b)
+		if result != table.r {
+			t.Errorf(
+				"Vector sum calculated wrongly; expected %v, received %v",
+				table.r,
+				result,
+			)
+		}
+	}
+}
+
+func TestFromAtoB(t *testing.T) {
+	tables := []struct {
+		a Vec
+		b Vec
+		r Vec
+	}{
+		{zero, vecA, vecA},
+		{vecA, vecA, zero},
+		{vecA, vecB, Vec{-1, 1}},
+		{vecB, vecA, Vec{1, -1}},
+		{vecA, vecC, Vec{-5.4, 1.5}},
+	}
+
+	for _, table := range tables {
+		result := FromAtoB(table.a, table.b)
+		if result != table.r {
+			t.Errorf(
+				"Vector TestFromAtoB calculated wrongly; expected %v, received %v",
+				table.r,
+				result,
+			)
+		}
+	}
+}
+
+func TestDot(t *testing.T) {
+	tables := []struct {
+		a Vec
+		b Vec
+		x float64
+	}{
+		{unitX, unitY, 0}, // orthogonal vectors zero
+		{vecA, vecA, 5},
+		{vecA, vecB, 4},
+		{vecB, vecA, 4},
+		{vecA, vecC, -4.3},
+	}
+
+	for _, table := range tables {
+		result := Dot(table.a, table.b)
+		if result != table.x {
+			t.Errorf(
+				"Dot product calculated wrongly; expected %f, received %f",
+				table.x,
+				result,
+			)
+		}
+	}
+}
+
 func TestDet(t *testing.T) {
 	tables := []struct {
 		a Vec
 		b Vec
 		x float64
 	}{
-		{
-			Vec{2, 1},
-			Vec{2, 1},
-			0,
-		},
-		{
-			Vec{2, 1},
-			Vec{1, 2},
-			3,
-		},
-		{
-			Vec{1, 2},
-			Vec{2, 1},
-			-3,
-		},
+		{unitX, unitY, 1},     //orthogonal vectors: product of norms
+		{vecA, vecA, 0},       // identical vectors: zero
+		{unitX, Vec{2, 0}, 0}, // collinear vectors: zero
+		{vecA, vecB, 3},
+		{vecB, vecA, -3},
+		{vecA, vecC, 8.4},
 	}
 
 	for _, table := range tables {
@@ -44,6 +117,31 @@ func TestDet(t *testing.T) {
 		if result != table.x {
 			t.Errorf(
 				"Determinant calculated wrongly; expected %f, received %f",
+				table.x,
+				result,
+			)
+		}
+	}
+}
+
+func TestNormSquare(t *testing.T) {
+	tables := []struct {
+		a Vec
+		x float64
+	}{
+		{zero, 0},
+		{unitX, 1},
+		{unitY, 1},
+		{vecA, 5},
+		{vecB, 5},
+		{vecC, 17.81},
+	}
+
+	for _, table := range tables {
+		result := NormSquare(table.a)
+		if result != table.x {
+			t.Errorf(
+				"Norm square calculated wrongly; expected %f, received %f",
 				table.x,
 				result,
 			)
