73b481b2532a63939c302df6b7126c445d243fee
gerald schenk <gschenk@gmx.net>
2020-04-15 17:38:30 +0200

    adds methods for Vectors, Norms, Norm2s

--- vector/vec.go
+++ vector/vec.go
@@ -19,3 +19,19 @@ func (v Vec) Norm2() float64 { return normSquare(v) }
 func (vs Vecs) Foldr(f func(Vec, Vec) Vec, v0 Vec) Vec {
 	return foldrVecs(f, vs, v0)
 }
+
+// Norms returns the norm of a vector
+func (vs Vecs) Norms() []float64 {
+	return mapScalarFunToVecs(
+		func(v Vec) float64 { return v.Norm() },
+		vs,
+	)
+}
+
+// Norm2s returns the squared norm of a vector (dot product with itself)
+func (vs Vecs) Norm2s() []float64 {
+	return mapScalarFunToVecs(
+		func(v Vec) float64 { return v.Norm2() },
+		vs,
+	)
+}
